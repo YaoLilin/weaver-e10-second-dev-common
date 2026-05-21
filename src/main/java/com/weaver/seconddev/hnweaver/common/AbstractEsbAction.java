1800067a436bdf1a983f390bf0d871c56cc943cd
yaolilin <yaolilin@outlook.com>
2026-05-21 11:24:05 +0800

    refactor(AbstractEsbAction):增加日志和文档注释

--- src/main/java/com/weaver/seconddev/hnweaver/common/AbstractEsbAction.java
+++ src/main/java/com/weaver/seconddev/hnweaver/common/AbstractEsbAction.java
@@ -25,8 +25,10 @@ public abstract class AbstractEsbAction <T,R> implements EsbServerlessRpcRemoteI
             T paramObj = convertToParamObj(params);
             WeaResult<R> result = doExecute(paramObj);
             if (result.isFail()) {
+                log.error("执行失败，错误信息：{}", result.getMsg());
                 return WeaResult.fail(result.getMsg());
             }
+            log.info("执行成功，结果：{}", JSONObject.toJSONString(result));
             return WeaResult.success(Convert.toMap(String.class, Object.class, result.getData()));
         } catch (Exception e) {
             log.error("执行失败", e);
@@ -53,7 +55,9 @@ public abstract class AbstractEsbAction <T,R> implements EsbServerlessRpcRemoteI
     protected abstract T convertToParamObj(Map<String, Object> params);
 
     /**
-     * 将 map 转换为 java 对象
+     * 将 map 转换为 java 对象。<br>
+     * 注意，如果 Action 传入参数中包含 JSON 对象或数组，不可使用本方法进行转换，会导致报错，因为 JSON 对象或数组
+     * 在 Action 中会被转为字符串。
      *
      * @param params action 传入的参数
      * @param clazz  参数对象类型
